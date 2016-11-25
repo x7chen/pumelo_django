62a6c6c2632d382ece1d89029bf0ad73760ac2c5
sean <x7chen@126.com>
2016-11-26 00:02:26 +0800

    models

--- eslone/models.py
+++ eslone/models.py
@@ -15,9 +15,29 @@ class Goods(models.Model):
 
 
 class GoodLogs(models.Model):
-    good_id = models.IntegerField(default=0)
+    goods_id = models.IntegerField(default=0)
     item = models.CharField(max_length=50)
     before = models.CharField(max_length=200)
     after = models.CharField(max_length=200)
-    data = models.CharField(max_length=200)
+    date = models.CharField(max_length=200)
     operator = models.CharField(max_length=200)
+
+
+class Esls(models.Model):
+    label_num = models.CharField(max_length=20)
+    goods_id = models.IntegerField(default=0)
+    location = models.CharField(max_length=200)
+    label_type = models.CharField(max_length=20)
+    update_time = models.CharField(max_length=50)
+
+
+class ChangeList(models.Model):
+    label_id = models.IntegerField(default=0)
+    indate = models.CharField(max_length=50)
+
+
+class TaskLogs(models.Model):
+    change_list_id = models.IntegerField(default=0)
+    result = models.CharField(max_length=20)
+    notes = models.CharField(max_length=1000)
+
