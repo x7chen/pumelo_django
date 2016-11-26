f5f19f8d8a6177535dad6d7949e3ff79244401cc
root <root@localhost.localdomain>
2016-11-26 11:22:43 +0800

    combine

--- eslone/views.py
+++ eslone/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse(u'''{
+"employees": [
+{ "firstName":"Bill" , "lastName":"Gates" },
+{ "firstName":"George" , "lastName":"Bush" },
+{ "firstName":"Thomas" , "lastName":"Carter" }
+]
+}''')
--- pumelo/urls.py
+++ pumelo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from eslone import views as eslone_views
 urlpatterns = [
+    url(r'^$',eslone_views.index),
     url(r'^admin/', admin.site.urls),
 ]
