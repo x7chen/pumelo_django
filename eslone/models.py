9d805cac9788454682b513fbf52810ad75a63d60
x7chen@126.com <x7chen@126.com>
2016-11-26 11:25:44 +0800

    add dbtest

--- eslone/models.py
+++ eslone/models.py
@@ -13,6 +13,9 @@ class Goods(models.Model):
     barcode = models.CharField(max_length=50)
     level = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return self.name
+
 
 class GoodLogs(models.Model):
     goods_id = models.IntegerField(default=0)
@@ -22,6 +25,9 @@ class GoodLogs(models.Model):
     date = models.CharField(max_length=200)
     operator = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.item
+
 
 class Esls(models.Model):
     label_num = models.CharField(max_length=20)
@@ -30,14 +36,23 @@ class Esls(models.Model):
     label_type = models.CharField(max_length=20)
     update_time = models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return self.label_num
+
 
 class ChangeList(models.Model):
     label_id = models.IntegerField(default=0)
     indate = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.label_id
 
 
 class TaskLogs(models.Model):
     change_list_id = models.IntegerField(default=0)
     result = models.CharField(max_length=20)
     notes = models.CharField(max_length=1000)
+
+    def __unicode__(self):
+        return self.change_list_id
 
--- eslone/tests.py
+++ eslone/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.http import HttpResponse
+from models import Goods
+
 
 # Create your tests here.
+def db_add(request):
+    goods = Goods
+    goods.name = u"柚子"
+    goods.save()
+    return HttpResponse(u"添加成功")
+
+
+def db_read(reqest):
+    goods = Goods.objects.get(0)
+    return HttpResponse(goods.name)
