eff82a8b588cb58e251c45929b0a24fe634474b5
x7chen@126.com <x7chen@126.com>
2016-11-26 11:34:25 +0800

    add dbtest1

--- eslone/tests.py
+++ eslone/tests.py
@@ -6,9 +6,9 @@ from models import Goods
 # Create your tests here.
 def db_add(request):
     goods = Goods
-    goods.name = u"柚子"
+    goods.name = "柚子"
     goods.save()
-    return HttpResponse(u"添加成功")
+    return HttpResponse("添加成功")
 
 
 def db_read(reqest):
