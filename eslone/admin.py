0f6c852b3d8f1e4700d8d621f3eaf2c8eae1e18f
x7chen@126.com <x7chen@126.com>
2016-12-01 14:46:58 +0800

    1201

--- eslone/admin.py
+++ eslone/admin.py
@@ -4,7 +4,10 @@ from models import *
 
 # Register your models here.
 class GoodsAdmin(admin.ModelAdmin):
+    # 可编辑的表单
     fields = ['name', 'price', 'package', 'origin', 'barcode']
+    # 显示的字段
+    list_display = ('name', 'price', 'package', 'origin', 'barcode')
 
 
 admin.site.register(Goods, GoodsAdmin)
