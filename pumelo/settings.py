8400cf183f9eeaa4653bd656a720a5a4de495a90
x7chen@126.com <x7chen@126.com>
2016-12-01 16:48:41 +0800

    1201

--- pumelo/settings.py
+++ pumelo/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
