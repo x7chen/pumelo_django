37c16e4a10c5af9a035fd119e3b8d7a5c4e5f703
x7chen@126.com <x7chen@126.com>
2016-12-01 14:38:46 +0800

    1201

--- eslone/admin.py
+++ eslone/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from models import *
 
-# Register your models here.
 
-admin.site.register(Goods)
+# Register your models here.
+class GoodsAdmin(admin.ModelAdmin):
+    fields = ['name', 'price', 'package', 'origin', 'barcode']
+
+
+admin.site.register(Goods, GoodsAdmin)
 admin.site.register(GoodLogs)
