bba28fbafa17c020565c8416700115a34761e043
x7chen@126.com <x7chen@126.com>
2016-12-29 16:59:45 +0800

    1229

--- eslone/admin.py
+++ eslone/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
 from models import *
+import datetime
 
 
 # Register your models here.
 class GoodsAdmin(admin.ModelAdmin):
     list_filter = ['barcode']
     search_fields = ['barcode']
-#   fields = ['name', 'price', 'package', 'origin', 'barcode']
+    #   fields = ['name', 'price', 'package', 'origin', 'barcode']
     list_display = ('name', 'price', 'package', 'origin', 'barcode')
+
+    def save_model(self, request, obj, form, change):
+        super(GoodsAdmin, self).save_model(request, obj, form, change)
+        goods_log = GoodLogs
+        if change:
+            obj_original = self.model.objects.get(pk=obj.pk)
+            goods_log.goods = obj.id
+            goods_log.item = "price"
+            goods_log.before = obj_original.price
+            goods_log.after = obj.price
+            goods_log.date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            goods_log.save()
+        else:
+            goods_log.goods = obj.id
+            goods_log.item = "price"
+            goods_log.before = "-"
+            goods_log.after = obj.price
+            goods_log.date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            goods_log.save()
+            return
 
 
 admin.site.register(Goods, GoodsAdmin)
